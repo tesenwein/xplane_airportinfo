de22fba7cb102c06e9bff0470933742b0733afe4
Theodor Esenwein <theo@esenwein.ch>
2018-03-18 21:11:27 +0100

    close

--- PI_AirportInfo.py
+++ PI_AirportInfo.py
@@ -152,16 +152,15 @@ class PythonInterface:
 
 	def XPluginStart(self):
 
-		is_env_ok()
-
 		self.Name = "Aiport Info" + VERSION
 		self.Sig =  "TheoEsenwein.Python.AiportInfo"
 		self.Desc = "A plugin to get some Aiport information."
 		self.AirportWindowCreated = False
 		self.current_airport_icao = ""
 		self.current_airport_name = ""
-		self.current_airport_info = ""
 		self.current_airport_metar = None
+		self.current_airport_runways = None
+		self.current_aiprot_openrunway = None
 		
 		self.AirportMenuCB = self.AMHandler
 		self.mPluginItem = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "Aiport Info", 0, 1)
@@ -261,16 +260,15 @@ class PythonInterface:
 		XPSetWidgetDescriptor(self.AirportIcao, str(self.current_airport_icao))
 
 		# Show Result		
-		top_row -= row_h
-		self.InfoTitle = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h, 1, "Current Information", 0, self.AirportWindow, xpWidgetClass_Caption)
-		top_row -= row_h
-		self.InfoRow1 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h, 1, "Airport:", 0, self.AirportWindow, xpWidgetClass_Caption)
+		top_row -= row_h	
+		self.InfoRow1 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h, 1, "", 0, self.AirportWindow, xpWidgetClass_Caption)
 		top_row -= row_h2
-		self.InfoRow2 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h2, 1, "QNH:", 0, self.AirportWindow, xpWidgetClass_Caption)
+		self.InfoRow2 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h2, 1, "", 0, self.AirportWindow, xpWidgetClass_Caption)
 		top_row -= row_h2
-		self.InfoRow3 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h2, 1, "WIND:", 0, self.AirportWindow, xpWidgetClass_Caption)
-
-
+		self.InfoRow3 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h2, 1, "", 0, self.AirportWindow, xpWidgetClass_Caption)
+		top_row -= row_h2
+		self.InfoRow4 = XPCreateWidget(left_col_1, top_row,right_col_3, top_row - row_h2, 1, "", 0, self.AirportWindow, xpWidgetClass_Caption)
+	
 		# Register the widget handler
 		self.AMHandlerCB = self.AWHandler
 		XPAddWidgetCallback(self, self.AirportWindow, self.AMHandlerCB)
@@ -292,6 +290,8 @@ class PythonInterface:
 
 		XPGetWidgetDescriptor(self.AirportIcao, out_icao_name, 20)
 		self.current_airport_icao = out_icao_name[0]
+
+		self.current_airport_icao = "LSZH"
 		
 		# search for the information
 		if(len(self.current_airport_icao) < 4):
@@ -304,15 +304,55 @@ class PythonInterface:
 
 		self.current_airport_metar = Route_Finder.get_airportweather_icao(self.current_airport_icao)
 
-		self.print_airport_info()
-
-	def print_airport_info(self):
-		XPSetWidgetDescriptor(self.InfoRow1, "Airport: " +  str(self.current_airport_name) + "(" + str(self.current_airport_icao) + ")")
+		AirportOb = Airport(self.current_airport_icao)
+		self.current_airport_runways = AirportOb.runways
 
 		if(self.current_airport_metar):
-			XPSetWidgetDescriptor(self.InfoRow2, "QNH: " +  str(self.current_airport_metar.press))
-			XPSetWidgetDescriptor(self.InfoRow3, "WIND: " + str(self.current_airport_metar.wind_dir) + "/" +  str(self.current_airport_metar.wind_speed))			
+			self.current_aiprot_openrunway = AirportOb.determine_open_runway(self.current_airport_metar.wind_dir.value())
 
+		self.print_airport_info()
+
+	def get_runway_info(self, runway_id):
+		return self.current_airport_runways[str(runway_id)]
+
+	def print_airport_info(self):
+		XPSetWidgetDescriptor(self.InfoRow1, "Airport: " +  str(self.current_airport_name) + " (" + str(self.current_airport_icao) + ")")
+
+		if(self.current_airport_metar):
+			XPSetWidgetDescriptor(self.InfoRow2, "qnh: {} / {}".format(self.current_airport_metar.press.string("mb"),self.current_airport_metar.press.string("in")))
+			XPSetWidgetDescriptor(self.InfoRow3, "Wind: " + str(self.current_airport_metar.wind_dir) + " / " + self.current_airport_metar.wind())			
+		
+
+		if(self.current_aiprot_openrunway):
+			open_runway = self.get_runway_info(self.current_aiprot_openrunway)
+		
+		# Get all Runways
+		if(self.current_airport_runways):
+			runway_strresult = ""
+			for runway_info in self.current_airport_runways:			
+				prefix = ""
+				if(runway_info == self.current_aiprot_openrunway):
+					prefix = "*"
+
+				runway_strresult += prefix + self.get_runway_str(self.get_runway_info(runway_info))
+				
+			XPSetWidgetDescriptor(self.InfoRow4, runway_strresult)
+
+	def get_runway_str(self, runway):
+			
+		if(runway.ils != "0.000"):
+			return "Rwy: {}({}) ILS: {}({}) FT: {}".format(runway.id, 
+															runway.hdg,
+															runway.ils,
+															runway.ilscrs,
+															runway.length)
+		else:
+			return "Rwy: {}({}) FT: {}".format(runway.id, 
+												runway.hdg,
+												runway.length)
+		return None
+			
+			
 class Route(object):
 
 	def call_lan_lot(self):
@@ -415,5 +455,74 @@ class Weather(object):
 		if(self.metarcode):
 			self.data = Metar.Metar(self.metarcode)
 
+class Runway(object):
+	
+	def __init__(self, id, hdg, ils, ilscrs, length):
 
-		
+		self.id = id
+		self.hdg = hdg
+		self.ils = ils
+		self.ilscrs = ilscrs
+		self.length = length
+
+class Airport(object):
+
+	def __init__(self, icao_code):
+
+		self.icao = icao_code
+		self.directories = is_env_ok()
+		self.airports_file_path = self.directories[4]
+		self.runways = None
+
+		self.read_runway_information()
+
+	def read_runway_information(self):
+		apt_lat_lon_elev = {}
+		runways = {}
+		with open(self.airports_file_path, 'r') as f:
+			lines = f.readlines()
+			is_needed_apt = False
+			for no, line in enumerate(lines):
+				if line.startswith("A,%s," % self.icao.upper()):
+					apt_info = line.strip().split(',')
+					apt_lat_lon_elev = {
+						"lat": float(apt_info[3]),
+						"lon": float(apt_info[4]),
+						"elev": float(apt_info[5])
+					}
+					is_needed_apt = True
+					continue
+				if is_needed_apt:
+					if line.strip().replace('\r', '').replace('\n', ''):
+						rwy_info = line.strip().split(',')
+						rwy_id = rwy_info[1]
+						rwy_hdg = rwy_info[2]
+						rwy_length = rwy_info[3]
+						rwy_ils = rwy_info[6]
+						rwy_ilscrs = rwy_info[7]
+						runways.update(
+							{
+								rwy_hdg: Runway(rwy_id,rwy_hdg,rwy_ils,rwy_ilscrs,rwy_length)
+							}
+						)
+					else:
+						break
+
+		self.runways = runways
+
+	def determine_open_runway(self, wind):
+
+		runways_simple = []
+
+		for runway in self.runways:
+			runways_simple.append(int(runway))
+
+		runway_dir = min(runways_simple, key=lambda x:abs(x-int(wind)))
+
+		for runway in self.runways:
+
+			if(int(self.runways[runway].hdg) == runway_dir):
+				return self.runways[runway].hdg 
+
+
+		return None
